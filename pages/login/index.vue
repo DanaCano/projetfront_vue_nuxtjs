59c3dfdcfc335df006580396508d12fad8268eb8
RAKOTOARISOA Tsiky <tsihafaandry@gmail.com>
2022-02-18 15:11:00 +0300

    add verify plugin

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -14,28 +14,41 @@
       <div class="content1-form-login">
         <form action="" @submit.prevent>
           <label class="form-label">USERNAME / MAIL</label>
-          <div class="field">
-            <i class="icon-arobase"></i>
-            <input
-              type="text"
-              class="form-control"
-              required
-              placeholder="Write your username here..."
-              v-model="login.username"
-            />
-          </div>
+
+          <ValidationProvider rules="required" v-slot="{ errors }">
+            <div class="field">
+              <i class="icon-arobase"></i>
+              <input
+                type="text"
+                class="form-control"
+                required
+                placeholder="Write your username here..."
+                v-model="login.username"
+              />
+            </div>
+            <span>{{ errors[0] }}</span>
+          </ValidationProvider>
           <label class="form-label">PASSWORD</label>
-          <div class="field">
+          <ValidationProvider rules="required" v-slot="{ errors }">
+            <div class="field">
               <i class="icon-lock-alt"></i>
-            <input
-              type="password"
-              required
-              class="form-control"
-              placeholder="Write your passwword here..."
-              v-model="login.password"
-            />
-          </div>
-          <span class="mini-label"><NuxtLink to="/recuperation" name="resetpassword">Forgot your password?</NuxtLink></span>
+
+              <input
+                type="password"
+                required
+                class="form-control"
+                placeholder="Write your passwword here..."
+                v-model="login.password"
+              />
+
+              <span>{{ errors[0] }}</span>
+            </div>
+          </ValidationProvider>
+          <span class="mini-label"
+            ><NuxtLink to="/recuperation" name="resetpassword"
+              >Forgot your password?</NuxtLink
+            ></span
+          >
 
           <div class="form-group top">
             <button class="form-btn" @click="onLogin()">log in</button>
@@ -49,24 +62,29 @@
 <script lang="ts">
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
+import { ValidationObserver, ValidationProvider } from "vee-validate";
 @Component({
   layout: "login",
+  components: {
+    ValidationObserver,
+    ValidationProvider,
+  },
 })
 class Login extends Vue {
-  login: any = {username: '', password: ''}
-  mounted () {
+  login: any = { username: "", password: "" };
+  mounted() {
     if (this.$auth.loggedIn) {
-      this.$router.push('/dashboard')
+      this.$router.push("/dashboard");
     }
   }
   async onLogin() {
     try {
-      const log = await this.$auth.loginWith('local', {data: this.login})
-      if(this.$auth.loggedIn) {
-        this.$router.push('/dashboard')
+      const log = await this.$auth.loginWith("local", { data: this.login });
+      if (this.$auth.loggedIn) {
+        this.$router.push("/dashboard");
       }
-    } catch(err) {
-      console.error(err)
+    } catch (err) {
+      console.error(err);
     }
   }
 }
