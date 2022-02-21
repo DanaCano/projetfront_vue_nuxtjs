58e3eac87ec4199fd3da04416e38b037fdf0892b
RAKOTOARISOA Tsiky <tsihafaandry@gmail.com>
2022-02-21 15:54:02 +0300

    language switcher ok

--- components/i18n/languageSwitcher.vue
+++ components/i18n/languageSwitcher.vue
@@ -1,22 +1,36 @@
 <template>
   <div class="language-switcher">
-    <div>
-      <i class="fflag fflag-FR ff-xl ff-round"></i>
+    <div class="language-button" @click="showSwitcher = !showSwitcher">
+      <i
+        class="fflag ff-xl ff-round"
+        v-bind:class="`fflag-${locale.toUpperCase()}`"
+      ></i>
     </div>
-    <div class="language-list">
+    <div class="language-list" v-if="showSwitcher">
       <div v-for="lang in $i18n.locales" :key="lang.code" class="language-item">
-        {{lang.name}}
+        <span @click="locale = lang.code">
+          <nuxt-link :to="switchLocalePath(lang.code)">
+            <i
+              class="fflag ff-xl ff-round"
+              :class="`fflag-${lang.code.toUpperCase()}`"
+            ></i>
+            {{ lang.name }}
+          </nuxt-link>
+        </span>
       </div>
     </div>
   </div>
 </template>
 <script lang="ts">
-import Vue from 'vue'
-import Component from 'vue-class-component'
+import Vue from "vue";
+import Component from "vue-class-component";
 @Component
 class LanguageSwitcher extends Vue {
-  mounted () {
+  showSwitcher: boolean = false;
+  locale: string = ''
+  mounted() {
+    this.locale = this.$i18n.defaultLocale
   }
 }
-export default LanguageSwitcher
+export default LanguageSwitcher;
 </script>
