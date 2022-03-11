d22d3829c10a6fa7c7130f4efce87e2c180e0103
RAKOTOARISOA Tsiky <tsihafaandry@gmail.com>
2022-03-11 11:05:58 +0300

    document init

---
+++ pages/document.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Document</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+@Component ({
+  layout: 'connected',
+  middleware: ['auth']
+})
+
+class Document extends Vue {
+
+}
+export default Document
+</script>
